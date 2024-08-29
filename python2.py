e4c38f0471f9cc5ce715ca6970503bd30046a669
syeda-ashna <syedabattool@gmail.com>
2024-08-29 09:32:21 -0700

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,59 @@
+# current and birthyear
+def age(birth_year:int, current_year:int):
+    age:int = current_year-birth_year
+    print(age)
+age(2006,2024)
+
+# area of circle
+def area(length:int, width:int):
+    area:int = length*width
+    print(area)
+area(10,5)
+
+
+# tem from F to C
+def f(celsius:int, ):
+ f=celsius*(9/5)+32
+ print(f)
+f(0)
+
+# area of a circle
+def area(r:int,): 
+ area=3.14*r*r
+ print(area)
+area(5)
+
+# percentage
+def per(a:int,b:int,c:int,d:int,total:int):
+ obtained=a+b+c+d
+ per=obtained/total
+ print(per)
+per(5,10,5,10,100)
+
+# volume of cylinder
+def v(r:int,h:int,):
+ v=3.14*r*r*h
+ print(v)
+v(5,10)
+
+# area of cube
+def A(a:int,):
+ A=6*a*a
+ print(A)
+A(5)
+
+# BMI
+def bmi(weight:int, height:int):
+ bmi=weight/height*height
+ print(bmi)
+ bmi(60,20)
+
+ # given number of second into minute
+def minsec(m:int,s:int,):
+ min=s/60
+ sec=m*60
+ print(min)
+ print(sec)
+minsec(5,10)
+
+
